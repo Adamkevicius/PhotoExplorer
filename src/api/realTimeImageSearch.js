27316ec7c00c04de9f2314f3ad0f92f28d55dc91
Adamkevicius <imatveyadam@gmail.com>
2025-11-28 18:21:40 +0200

    add api config file for external service integrations

--- src/api/realTimeImageSearch.js
+++ src/api/realTimeImageSearch.js
@@ -1,10 +1,11 @@
+import { rapidApiRealTimeImageSearch } from "../../constants";
 
 const BASE_URL = "https://real-time-image-search.p.rapidapi.com"
 const options = {
     method: 'GET',
 	headers: {
-		'x-rapidapi-key': 'e2a058d304msh9d0a4a435534e52p19c164jsn5e1fbcb6803a',
-		'x-rapidapi-host': 'real-time-image-search.p.rapidapi.com'
+		'x-rapidapi-key': rapidApiRealTimeImageSearch.rapidApiKey,
+		'x-rapidapi-host': rapidApiRealTimeImageSearch.rapidApiHost
 	}
 }
 
@@ -12,11 +13,10 @@ export const fetchImages = async (name, limit, size, fileType) => {
     const url = `${BASE_URL}/search?query=${name}&limit=${limit}&size=${size}&color=any&type=any&time=any&usage_rights=any&file_type=${fileType}&aspect_ratio=any&safe_search=on&region=us`
 
     try {
-	const response = await fetch(url, options);
-	const result = await response.json();
+        const response = await fetch(url, options);
+        const result = await response.json();
     
-    console.log(result)
-    return result
+        return result
     } catch (error) {
         console.error(error);
     }
