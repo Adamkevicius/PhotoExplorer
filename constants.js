24c400e90dfcb91d04f392629450b0d866c9615a
Adamkevicius <imatveyadam@gmail.com>
2025-11-28 22:28:10 +0200

    fix

---
+++ constants.js
@@ -0,0 +1,12 @@
+export const appwriteConfig = {
+    endpoint: "https://fra.cloud.appwrite.io/v1",
+    projectId: "692569050005ac0c62da",
+    databaseId: "6926e287003d6f6b46e9",
+    bucketId: "6926e3ac00056c24a605",
+    imagesCollectionId: "image"
+}
+
+export const rapidApiRealTimeImageSearch = {
+    rapidApiKey: '131ca3060dmsh1dc4021bfe20266p1186a2jsnddb5c8a97050',
+    rapidApiHost: 'real-time-image-search.p.rapidapi.com'
+}
